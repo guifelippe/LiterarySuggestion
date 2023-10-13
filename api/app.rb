e2f212864fcf5265106165b68eed95e2960779d5
Guilherme Felippe <guifelippe3005@gmail.com>
2023-10-12 22:34:18 -0300

    created api

---
+++ api/app.rb
@@ -0,0 +1,66 @@
+require 'sinatra'
+require 'json'
+require 'pg' # Driver do PostgreSQL
+require 'rack/cors' # Adicione esta linha para carregar o rack-cors
+
+use Rack::Cors do
+  allow do
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: [:get, :post, :options]
+  end
+end
+
+db = PG.connect(dbname: 'literaly', user: 'postgres', password: 'g33847834')
+
+get '/books' do
+  content_type :json
+  result = db.exec('SELECT * FROM books')
+  result.map { |row| row.to_h }.to_json
+end
+
+get '/books/:genreId' do
+  content_type :json
+  genre_id = params[:genreId]
+  result = db.exec_params('SELECT * FROM books WHERE "genreId" = $1', [genre_id])
+  books = result.map { |row| row.to_h }
+  books.to_json
+end
+
+get '/genre' do 
+    content_type :json
+    result = db.exec_params('SELECT * FROM genre')
+    result.map { |row| row.to_h }.to_json
+end
+
+post '/genre-create' do
+    genre_data = JSON.parse(request.body.read)
+    
+    if genre_data.key?('name')
+      name = genre_data['name']
+      
+      result = db.exec_params('INSERT INTO genre (name) VALUES ($1) RETURNING *', [name])
+      
+      result.first.to_h.to_json
+    else
+      status 400
+      body 'The "name" field is mandatory.'
+    end
+end
+
+post '/books-create' do
+  book_data = JSON.parse(request.body.read)
+  
+  if book_data.key?('title') && book_data.key?('image') && book_data.key?('summary') && book_data.key?('genreId')
+      title = book_data['title']
+      image = book_data['image']
+      summary = book_data['summary']
+      genre_id = book_data['genreId']
+    
+      result = db.exec_params('INSERT INTO books (title, image, summary, "genreId") VALUES ($1, $2, $3, $4) RETURNING *', [title, image, summary, genre_id])
+    
+      result.first.to_h.to_json
+  else
+      status 400
+      body 'Os campos "title", "image", "summary" e "genreId" são obrigatórios.'
+  end
+end
---
+++ src/components/Home.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+import axios from 'axios';
+
+const Home: React.FC = () => {
+  const [genre, setGenre] = useState<string>('');
+  const [genres, setGenres] = useState<{ id: number; name: string }[]>([]);
+  const [randomBook, setRandomBook] = useState<{ id: number, title: string, image: string, summary: string, genreId: number } | null>(null);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    axios.get('http://localhost:4567/genre')
+    .then((response) => setGenres(response.data))
+    .catch((error) => alert(`Error when updating genres: ${error}`))
+  }, [])
+
+  const getRandomBook = (genreId: number) => {
+    if(!genreId){
+        setError('Please select a genre.');
+        return;
+    }
+
+    setError('')
+
+    axios.get(`http://localhost:4567/books/${genreId}`)
+    .then((response) => {
+        if(response.data.length === 0){
+            setError('There are no books available for the selected genre.');
+            return;
+        }
+
+        const randomIndex = Math.floor(Math.random() * response.data.length);
+        const randomBookInfo = response.data[randomIndex];
+
+        setRandomBook(randomBookInfo)
+    })
+    .catch((error) => alert(`Error when searching for book: ${error}`))
+  };
+
+  return (
+    <div className="home">
+      <h1>Book Recommender</h1>
+      <div>
+        <label>Escolha um gênero de livro:</label>
+        <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+          <option value="">Selecione um gênero</option>
+          {genres.map((g) => (
+            <option key={g.id} value={g.id}>
+              {g.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button onClick={() => getRandomBook(parseInt(genre))}>Obter Recomendação</button>
+      {error && <p className="error-message">{error}</p>}
+      {randomBook && (
+        <div className="book-info">
+          <h2>Título: {randomBook.title}</h2>
+          <img className="responsive-image" src={randomBook.image} alt={randomBook.title} />
+          <p>Resumo:<br/> {randomBook.summary}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
